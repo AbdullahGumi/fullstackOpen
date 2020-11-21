5ca312c4dbdb20b9c16c485a51a5e9f31ed22447
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-22 00:35:36 +0100

    created Part component and finished exercise 1.2: course information

--- part1/course information/src/components/Content.js
+++ part1/course information/src/components/Content.js
@@ -1,17 +1,12 @@
 import React from 'react';
-
+import Part from './Part.js';
 const Content = ({ parts }) => {
   return (
   	<div>
       {parts ?
-        (parts.map(part => {
-          return (
-            <p>
-            	{part.name}{' '}
-            	{part.exercises}
-            </p>
-          );
-        })) : null 
+        (parts.map(part => (
+            <Part part={part.name} exercise={part.exercise} />
+        ))) : null 
     }
     </div>
   );
---
+++ part1/course information/src/components/Part.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Part = ( { part, exercise } ) => {
+	return (
+		<p>{part}{' '}{exercise}</p>
+	);
+}
+
+export default Part;
