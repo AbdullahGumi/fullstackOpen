72cb4d2737eb298bf786c206fa2de6c77debc5bf
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-23 20:22:22 +0100

    finished exercise 1.13: anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,14 +2,27 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = ({ anecdotes }) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
   const nextAnecdotes = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   }
+  
+  const voteForAnecdotes = () => {
+    const tmpVotes = [...votes];
+    tmpVotes[selected]++;
+    setVotes(tmpVotes);
+    console.log(tmpVotes);
+    console.log('votes', votes); 
+  }
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <li>{anecdotes[selected]}</li>
       <button onClick={nextAnecdotes}>View next anecdotes</button>
+      <button onClick={voteForAnecdotes}>Vote</button>
+      <p>This anecdotes has {votes[selected]} votes</p>
     </div>
   )
 }
