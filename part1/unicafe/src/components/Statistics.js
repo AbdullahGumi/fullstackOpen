b4cc2319d8be195a1dd11215e3ffc2cd239c838e
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-23 15:29:33 +0100

    finished exercise 1.9: unicafe

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 
-const Statistics = ({ good, neutral, bad }) => (
-	<div>
-      <h2>Statistics</h2>
-      <p>good: {good}</p> 
-      <p>Neutral: {neutral}</p> 
-      <p>bad: {bad}</p> 
-      <p>all: {good + neutral + bad}</p>
-      <p>average: {(good + neutral + bad) / 3}</p>
-      <p>positive: {(good * 100)/ (good + neutral + bad)} %</p>
-	</div>
-);
+const Statistics = ({ good, neutral, bad }) => {
+ 	const statisticsStatus = good && neutral && bad ? (		
+ 		<div>
+	      <h2>Statistics</h2>
+	      <p>good: {good}</p> 
+	      <p>Neutral: {neutral}</p> 
+	      <p>bad: {bad}</p> 
+	      <p>all: {good + neutral + bad}</p>
+	      <p>average: {(good + neutral + bad) / 3}</p>
+	      <p>positive: {(good * 100)/ (good + neutral + bad)} %</p>
+		</div>
+		) : (<p>No feedback given</p>)
+	return statisticsStatus;
+};
 
 export default Statistics;
