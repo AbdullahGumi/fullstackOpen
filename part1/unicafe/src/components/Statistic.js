47c84d4c908416086fdb1b70f54461ec5064e9b6
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-23 17:18:38 +0100

    finished exercise 1.11: unicafe

--- part1/unicafe/src/components/Statistic.js
+++ part1/unicafe/src/components/Statistic.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Statistic = ({ text, value }) => <p>{text}: {value}</p>;
+const Statistic = ({ text, value }) => (
+	<table>
+		<tbody>
+			<tr>
+				<td>{text}:</td> 
+				<td>{value}</td>
+			</tr>
+		</tbody>
+	</table>
+);
 
 export default Statistic; 
