c90176925d7bcbcc38fe9b1e4543abc03cd3fff5
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-24 11:02:59 +0100

    finished exercise 2.4 and 2.5

--- part2/course information/src/components/Course.js
+++ part2/course information/src/components/Course.js
@@ -3,12 +3,16 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({ course }) => (
-	<div>
-		<Header course={course.name}/>
-		<Content parts={course.parts} />
-	  	<Total parts={course.parts}/>
-	</div>
+const Course = ({ courses }) => (
+	courses.map(course => {
+		return (
+			<div key={course.id}>
+				<Header course={course.name}/>
+				<Content parts={course.parts} />
+			  	<Total parts={course.parts}/>
+			</div>
+		);
+	})
 );
 
 export default Course;
