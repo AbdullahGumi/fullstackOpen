66d9cd720066baca043d9e21e2acd687305a3444
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-24 10:12:37 +0100

    finished exercise 2.1

---
+++ part2/course information/src/components/Header.js
@@ -0,0 +1,7 @@
+	import React from 'react';
+
+const Header  = ({ course }) => (
+	<h1>{ course }</h1>
+);
+
+export default Header;
---
+++ part2/course information/src/components/Total.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Total = ({ parts }) => {
+	return (
+		<div>
+			{/*<h3>Total number of exercises: { parts[0].exercises + parts[1].exercises + parts[2].exercises }</h3>*/}
+		</div>
+		)
+};
+
+export default Total;
+
