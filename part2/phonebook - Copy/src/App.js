10ff462d2de9fe264ebdf64b18e0ffde157726a3
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-25 10:38:04 +0100

    finished exercise 2.11: modified the app such that the initial state of the data is fetched from a server using axios

--- part2/phonebook - Copy/src/App.js
+++ part2/phonebook - Copy/src/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]); 
-  const [ newName, setNewName ] = useState('');
-  const [ newNumber, setNewNumber ] = useState('');
-  const [ filteredName, setFilteredName ] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(res => setPersons(res.data))
-  }, [])
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filteredName, setFilteredName ] = useState('')
 
   const personName = persons.map(per => per.name);
 
