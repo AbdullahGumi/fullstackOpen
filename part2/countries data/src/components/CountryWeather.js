9f3a3f3c01368784f8d298fc57365f6bc6b2fff8
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-11-25 20:53:01 +0100

    changed API key and hid it

--- part2/countries data/src/components/CountryWeather.js
+++ part2/countries data/src/components/CountryWeather.js
@@ -6,7 +6,7 @@ const CountryWeather = ({ capitalName }) => {
 
 const [weatherData, setWeatherData] = useState([]);
 
-const key = '3f87fe8df6fbd3c99832d0d61826248f';
+const key = process.env.REACT_APP_NOT_SECRET_CODE;
 useEffect(() => {
 	axios
 		.get(`http://api.weatherstack.com/current?access_key=${key}&query=${capitalName}&units=f`)
@@ -15,7 +15,7 @@ useEffect(() => {
 }, [])
 
  return (
- 	<div> 
+ 	<div>
  		<h2>Weather in {capitalName}</h2>
 		<h3>Temperature: {Math.floor((weatherData.temperature - 32) * 5/9)} Celcius</h3>
 		<img alt='weather' src={weatherData.weather_icons}/>
