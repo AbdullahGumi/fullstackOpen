6529eab7ca5d9d735c076f36bc56d2ec5e50117b
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-03 21:54:23 +0100

    finished exercise 5.4: Implemented a notification component which informs a user about successful and unsuccessful operations

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -10,7 +10,8 @@ const App = () => {
   const [username, setUsername] = useState(''); 
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('')
+  const [message, setMessage] = useState('')
+  const [messageColor, setMessageColor] = useState('')
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [url, setUrl] = useState('');
@@ -34,11 +35,11 @@ const App = () => {
       setUsername('');
       setPassword('');
     } catch(e) {
-      setErrorMessage('Wrong Credentials');
+      setMessage('Invalid username or password');
+      setMessageColor('red')
       setTimeout(() => {
-        setErrorMessage(null);
+        setMessage(null);
       }, 5000)
-      console.log('execption', e);
     }
 
   }
@@ -51,6 +52,9 @@ const App = () => {
       url
     }
     blogService.createBlog(newBlog);
+    setMessage(`a new blog "${newBlog.title}" by ${newBlog.author} was added`)
+    setMessageColor('green')
+    setTimeout(() => setMessage(null), 5000)
   }
 
   useEffect(() => {
@@ -76,13 +80,11 @@ const App = () => {
 
   return (
     <div>
+      <Notfication  message={message} messageColor={messageColor}/>
     {user ? (
         <div>
           <h2>blogs</h2>
           <p>Logged in as {user.name}<button onClick={logout}>Logout</button></p>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
           <h2>Create your own blog!</h2>
           <form onSubmit={handleSubmit}>
             <div>
@@ -114,6 +116,9 @@ const App = () => {
             </div>            
             <button type="submit">create</button>
           </form>              
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
         </div>
       ) : (
         <div>
--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Notification = ({ errorMessage }) => {
+const Notification = ({ message, messageColor }) => {
+
+	const messageStyle = {
+		color: messageColor,
+		width: '100vw',
+		border: `3px solid ${messageColor}`,
+		backgroundColor: '#eee',
+		textAlign: 'center',
+		fontSize: '25px'
+
+	}
+
 	return (
 		<div>
-			<span>{errorMessage}</span>
+			{message &&
+				<div style={messageStyle}>
+					<span>{message}</span>
+				</div>				
+			}
 		</div>
 	);
 }
