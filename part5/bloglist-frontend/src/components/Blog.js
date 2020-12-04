3bb0c02aeb627057f2ad6735f87ef9990d4910ed
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-04 19:16:52 +0100

    finished exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
 
-export default Blog
+const [isView, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+const handleBlogView = () => {
+	setView(!isView);
+
+}
+
+const handleLike = () => {
+	console.log('Liked...')
+
+}
+
+	return (
+	  <div style={blogStyle}>
+	  	{isView ? (
+			  <div>
+			    <p>{blog.title}<button onClick={handleBlogView}>hide</button></p>
+			    <p>{blog.url}</p>
+			    <p>{blog.likes}<button onClick={handleLike}>like</button></p>
+			    <p>{blog.author}</p>
+			  </div>	  			
+	  		) : (
+			  <div>
+			    {blog.title} {blog.author}<button onClick={handleBlogView}>view</button>
+			  </div>	  		
+	  		)
+	  	}
+	  </div>
+	);
+}
+
+export default Blog;
