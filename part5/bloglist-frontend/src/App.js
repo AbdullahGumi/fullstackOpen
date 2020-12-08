476ef0121e5c6c531f5b528f7c04c542cb7b6711
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-08 14:45:01 +0100

    finished exercise 5.17

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -105,6 +105,7 @@ const App = () => {
             <div>
               username
                 <input
+                id='username'
                 type="text"
                 value={username}
                 name="Username"
@@ -114,13 +115,14 @@ const App = () => {
             <div>
               password
                 <input
+                id='password'
                 type="password"
                 value={password}
                 name="Password"
                 onChange={({ target }) => setPassword(target.value)}
               />
             </div>
-            <button type="submit">login</button>
+            <button id='login-button' type="submit">login</button>
           </form>          
         </div>
       )
