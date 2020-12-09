9a29d10d56cf5e3c8deb478de45fd49840a050b2
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-09 19:55:28 +0100

    finished exercise 6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
+import anecdoteService from './services/anecdotes';
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+	anecdoteService
+	.getAll()
+		.then(anec => anec.forEach(item => dispatch({ type: 'NEW_ANECDOTE', data: item })))
+	}, [dispatch])
 
 
   return (
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
 import { store } from './store';
 
 ReactDOM.render(
