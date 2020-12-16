d9847f64c76c2ef10366f61cdd5213e9b4999242
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-16 14:37:59 +0100

    finished exercise 8.2

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -89,17 +89,20 @@ const typeDefs = gql`
     published: String!
     author: String!
     id: ID!
+    genres: [String]!
   }  
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
 
   }
 }
