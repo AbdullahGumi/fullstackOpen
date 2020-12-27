42374fcf085be4490137b8674fed660c57518228
Abdullah_Gumi <abdulgumi77@gmail.com>
2020-12-27 22:09:02 +0100

    finished exercise 9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -10,8 +10,6 @@ export interface Diagnose {
   latin?: string;
 }
 
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -22,6 +20,58 @@ export interface Patient {
   entries: Entry[]
 }
 
+export enum EntryType {
+  HealthCheck = "HealthCheck",
+  OccupationalHealthCare = "OccupationalHealthcare",
+  Hospital = "Hospital",
+}
+
+interface BaseEntry {
+  id: string;
+  type: EntryType;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: EntryType.HealthCheck;
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: EntryType.OccupationalHealthCare;
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: EntryType.Hospital;
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthCareEntry
+  | HospitalEntry;
 
 export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
 export type NewPatientEntry = Omit<Patient, 'id'>;
